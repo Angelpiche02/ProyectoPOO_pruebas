8280ee219ab6c967af50921e0937e1ec10fe3c6c
Marco5074 <00114825@uca.edu.sv>
2026-04-01 12:59:29 -0600

    :sparkles: feat: Agregar atributos básicos a la clase Elemento

--- Proyecto/Proyecto/src/main/java/modelo/Elemento.java
+++ Proyecto/Proyecto/src/main/java/modelo/Elemento.java
@@ -2,6 +2,12 @@ package modelo;
 
 public class Elemento
 {
-    private int id ;
-    private String titulo; 
+    private int id;
+    private String titulo;
+    public tipoElemento tipo;
+    private String descripcion;
+    private String nivelPrioridad;
+
+    public static List<Elemento> elementos = new ArrayList<>();
+
 }
